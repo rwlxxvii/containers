b57508bf83b7ed533b5a51fc7c8eaaba6639fed4
Rob W <robertiweber3@gmail.com>
2023-12-07 22:06:44 -0600

    diff methods to query

--- devops-tools/fastapi/fastapi-with-mssql/sql_app/queries.py
+++ devops-tools/fastapi/fastapi-with-mssql/sql_app/queries.py
@@ -1,6 +1,12 @@
+import pandas as pd
 import pyodbc
-#import pymsmsql
+import os
+import sys
+import csv
+#import pymssql
 """
+following is example usage for connecting to a microsoft sql server, executing a query using cursor or pandas
+
 conn = pymssql.connect(
     server='<server-address>',
     user='<username>',
@@ -9,20 +15,21 @@ conn = pymssql.connect(
     as_dict=True
 )
 """
+
 SERVER = '<server-address>'
 DATABASE = '<database-name>'
 USERNAME = '<username>'
 PASSWORD = '<password>'
 
-connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
+connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD};Trusted_Connection=yes;'
 
 conn = pyodbc.connect(connectionString)
 
-SQL_QUERY = """
+SQL_QUERY1 = """
 SELECT 
 TOP 5 c.CustomerID, 
 c.CompanyName, 
-COUNT(soh.SalesOrderID) AS OrderCount 
+query1(soh.SalesOrderID) AS Orderquery1 
 FROM 
 SalesLT.Customer AS c 
 LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID 
@@ -30,8 +37,68 @@ GROUP BY
 c.CustomerID, 
 c.CompanyName 
 ORDER BY 
-OrderCount DESC;
+Orderquery1 DESC;
+"""
+
+SQL_QUERY2 = """
+USE master;
+SELECT name, is_disabled
+FROM sys.sql_logins
+WHERE principal_id = 1;
+"""
+
+SQL_QUERY3 = """
+USE master;
+SELECT *
+FROM sys.sql_logins
+WHERE [name] = 'sa' OR [principal_id] = 1;
+"""
+
+SQL_QUERY4 = """
+EXEC SP_CONFIGURE 'show advanced options', '1';
+     RECONFIGURE WITH OVERRIDE;
+     EXEC SP_CONFIGURE 'xp_cmdshell';
 """
 
 cursor = conn.cursor()
-cursor.execute(SQL_QUERY)
+cursor.execute(SQL_QUERY1)
+cursor.execute(SQL_QUERY2)
+cursor.execute(SQL_QUERY3)
+cursor.execute(SQL_QUERY4)
+
+# pandas sql query
+df = pd.read_sql_query('SELECT * FROM c.CustomerID', conn)
+print(df)
+print(type(df))
+
+# output to file
+query1 = SQL_QUERY1.fetchval()
+query2 = SQL_QUERY2.fetchval()
+f = open('/tmp/sql_query1.txt','a')
+print(query1)
+f.write('\n' + 'Top 5 Customer ID order query1:'+ str(query1))
+f.close()
+f = open('/tmp/sql_query2.txt','a')
+print(query2)
+f.write('\n' + 'Disabled Logins:'+ str(query2))
+f.close()
+
+# write to csv
+sql = """\
+USE master;
+SELECT name, is_disabled
+FROM sys.sql_logins
+WHERE principal_id = 1;
+SELECT *
+FROM sys.sql_logins
+WHERE [name] = 'sa' OR [principal_id] = 1;
+EXEC SP_CONFIGURE 'show advanced options', '1';
+     RECONFIGURE WITH OVERRIDE;
+     EXEC SP_CONFIGURE 'xp_cmdshell';
+"""
+rows = cursor.execute(sql)
+with open(r'/tmp/sql-queries.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow([x[0] for x in cursor.description])  # column headers
+    for row in rows:
+        writer.writerow(row)
