9bf3deb72010bbc958f0486e4a22a73bd79c7034
Rob W <robertiweber3@gmail.com>
2023-12-11 13:15:01 -0600

    diff methods to connect

--- devops-tools/fastapi/fastapi-with-mssql/sql_app/sqlaudit.py
+++ devops-tools/fastapi/fastapi-with-mssql/sql_app/sqlaudit.py
@@ -1,5 +1,8 @@
 import os
 import pyodbc
+#import urllib
+#from sqlalchemy import create_engine
+#from sqlalchemy import text
 
 SERVER = '<server-address>'
 DATABASE = '<database-name>'
@@ -9,6 +12,21 @@ PASSWORD = '<password>'
 connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD};Trusted_Connection=yes;'
 
 conn = pyodbc.connect(connectionString)
+
+#params = urllib.parse.quote_plus("DRIVER={ODBC Driver 18 for SQL Server};"
+#                                 "SERVER={SERVER};"
+#                                 "DATABASE={DATABASE};"
+#                                 "UID={USERNAME};"
+#                                 "PWD={PASSWORD}")
+
+#engine = create_engine("mssql+pyodbc:///?odbc_connect={}".format(params))
+
+#engine = create_engine('mssql+pyodbc://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}', echo=True)
+
+#with engine.connect() as con:
+#    with open("/tmp/sql/myquery.sql") as file:
+#        query = text(file.read())
+#        con.execute(query)
 
 # place sql file in this directory or change directory
 inputdir = 'C:\Users\Public\sqlaudit'
